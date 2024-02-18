ff2793d59c237938438e41f7ecb996dfa4207741
naimnaro <xez8jf@gmail.com>
2024-02-18 17:54:55 +0900

    sd
    asd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 
 function App() {
+
+  let post = '변수활용';
+  let [글제목, b] = useState(['블로그 제목1','블로그 제목2','블로그 제목3']);
+
   return (
     <div className="App">
       <div className ="black-nav">
         <h4> 리액트 리액트 야야야 아와와왕</h4>
-
+      </div>
+      <div className="list">
+        <h4> 아아아 </h4>
+      </div>
+      <div className="list">
+        <h4> 아아아2 </h4>
       </div>
     
     </div>
