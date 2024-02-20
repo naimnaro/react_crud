f2245b536c9cec707c4c7706e3c0c82cee5c7df6
naimnaro <xez8jf@gmail.com>
2024-02-20 20:26:58 +0900

    asd
    ads

--- src/login.js
+++ src/login.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(event){
+        event.prevenDefault();
+    }
+    
+
   return (
-    <div className='d-flex justify-content-center align-items-center bg-primary'>
+    <div className='d-flex vh-100 justify-content-center align-items-center bg-secondary'>
       <div className='p-3 bg-white w-25'>
-        <form action=''> 
+        <form onSubmit={handleSubmit}> 
             <div className='mb-3'>
                 <label htmlFor="email">Email</label>
-                <input type="email" placeholder='Enter Email' className='form-control'/>
+                <input type="email" placeholder='Enter email' className='form-control'
+                onChange={e => setEmail(e.target.value)}/>
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" placeholder='Enter Password'className='form-control'/>
+                <input type="password" placeholder='Enter password'className='form-control'
+                onChange={e => setPassword(e.target.value)}/>
             </div>
             <button className='btn btn-success'>Login</button>
         </form>
