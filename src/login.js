c5ee11b75a08b72d1c1730efb8460770f87a580b
naimnaro <xez8jf@gmail.com>
2024-02-20 20:01:09 +0900

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,19 +1,20 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function login() {
   return (
-    <div>
-      <div>
+    <div className='d-flex justify-content-center align-items-center bg-primary'>
+      <div className='p-3 bg-white w-25'>
         <form action=''> 
-            <div>
+            <div className='mb-3'>
                 <label htmlFor="email">Email</label>
-                <input type="email" placeholder='Enter Email'/>
+                <input type="email" placeholder='Enter Email' className='form-control'/>
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" placeholder='Enter Password'/>
+                <input type="password" placeholder='Enter Password'className='form-control'/>
             </div>
+            <button className='btn btn-success'>Login</button>
         </form>
       </div>
     </div>
