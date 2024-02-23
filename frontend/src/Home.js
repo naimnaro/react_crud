5afdcb7c4a0dbeb92faec28d79b66758dfe1369d
naim_naro <xez8jf@gmail.com>
2024-02-23 10:59:30 +0900

    asd
    asd

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
 
-function Home() {  return (    <div>Home</div>  )}
+function Home() {
+    const divStyle = {
+        fontSize: '60px', // 원하는 크기로 조정하세요
+        textAlign: 'center', // 텍스트를 가운데 정렬합니다
+        position: 'absolute', // 절대 위치로 설정합니다
+        top: '50%', // 수직 방향으로 중앙에 배치합니다
+        left: '50%', // 수평 방향으로 중앙에 배치합니다
+        transform: 'translate(-50%, -50%)', // 수평 및 수직으로 이동하여 정중앙에 배치합니다
+    };
+
+    return (
+        <div style={divStyle}>Welcome</div>
+    );
+}
+
 export default Home
