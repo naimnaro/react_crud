52ecaba2cb5536cb3b224747dee72c18fc9685a5
naimnaro <xez8jf@gmail.com>
2024-02-27 13:59:47 +0900

    asd
    asd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Signup from './Signup';
+import Login from './login';
+import Signup from './signup';
 import Home from './Home';
 
 function App() {
