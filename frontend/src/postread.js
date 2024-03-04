6aba9c917b992e8a99e2f5b48de3302cef1d397a
naimnaro <xez8jf@gmail.com>
2024-03-04 21:17:31 +0900

    asdsa
    saassdsd

--- frontend/src/postread.js
+++ frontend/src/postread.js
@@ -39,7 +39,10 @@ function PostRead({ user }) {
 
         fetchPost();
         fetchComments(); // fetchComments 함수를 여기서 호출
-    }, [post_id]);
+        if (user !== null) {
+            console.log("User:", user.name);
+        }
+    }, [post_id, user]);
 
     const handleCancel = () => {
         navigate('/pagenation');
@@ -81,13 +84,27 @@ function PostRead({ user }) {
                     </Table>
                     <ListGroup className="mb-3">
                         <Form.Label>댓글</Form.Label>
-                        {comments.map((comment, index) => (
+                        {comments.map((comment, index, post) => (
                             <ListGroup.Item key={index}>
                                 <div className="d-flex justify-content-between">
                                     <span><b>{comment.comment_name} </b>: {comment.content}</span>
                                     <div className="text-right">
                                         <span>{comment.created_at}</span>
-                                        
+                                        <span className="ms-4">
+                                            {user && user.name === comment.comment_name ? (
+                                                <>
+                                                    <Button variant="dark" size="sm" className="me-1">수정</Button>
+                                                    <Button variant="danger" size="sm" className="me-1">삭제</Button>
+                                                </>
+                                            ) : (
+                                                <> {/* 빈 요소를 렌더링하여 공간을 유지합니다 */}
+                                                    <Button variant="dark" size="sm" className="invisible">수정</Button>
+                                                    <Button variant="danger" size="sm" className="invisible">삭제</Button>
+                                                </>
+                                            )}
+
+                                        </span>
+
                                     </div>
                                 </div>
 
