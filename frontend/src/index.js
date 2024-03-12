686bf5d7270c93e9e94d572b875221e3343bdd11
naimnaro <xez8jf@gmail.com>
2024-03-12 13:29:41 +0900

    asd
    asd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactModal from 'react-modal';
 
 const root = createRoot(document.getElementById('root'));
+
+ReactModal.setAppElement(root);
+
 
 root.render(
   
--- frontend/src/post.js
+++ frontend/src/post.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import ReactModal from 'react-modal';
+
+// 모달 스타일을 설정합니다.
+
+ReactModal.setAppElement('#root');
 
 function PostForm({ user }) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const navigate = useNavigate();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
 
   useEffect(() => {
     console.log("User:", user);
@@ -15,13 +22,17 @@ function PostForm({ user }) {
     e.preventDefault();
     try {
       await axios.post('http://localhost:8081/post', { title, content, author_name: user.name });
-      alert('게시글이 성공적으로 작성되었습니다.');
-      // 게시글 작성 후 필요한 작업 수행
+      setModalMessage('게시글이 성공적으로 작성되었습니다.');
+      setModalIsOpen(true);
       navigate('/pagenation');
     } catch (error) {
       console.error('게시글 작성에 실패했습니다.', error);
       alert('게시글 작성에 실패했습니다.');
     }
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
   };
 
   const handletopostlist = () => {
@@ -41,13 +52,21 @@ function PostForm({ user }) {
               <label htmlFor="content" className="form-label">내용:</label>
               <textarea id="content" className="form-control" style={{ height: '400px' }} value={content} onChange={(e) => setContent(e.target.value)} />
             </div>
-            <div className="d-flex justify-content-end"> {/* 오른쪽으로 정렬하는 부분 */}
-              <button type="submit" className="btn btn-primary me-2">게시글 등록</button>
+            <div className="d-flex justify-content-end">
+              <button type="submit" className="btn btn-primary me-2"onClick={handleSubmit}>게시글 등록</button>
               <button type="button" className="btn btn-danger" onClick={handletopostlist}>취소</button>
             </div>
           </form>
         </div>
       </div>
+      {/* 모달 컴포넌트 */}
+      <ReactModal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="게시글 등록 결과">
+        <div>
+          <p>{modalMessage}</p>
+          <p>아아아아</p>
+          <button onClick={closeModal}>Close</button>
+        </div>
+      </ReactModal>
     </div>
   );
 }
