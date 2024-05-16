40162bca9d7266c7ef9fd1a310e69a6d7ef99459
naim_naro <xez8jf@gmail.com>
2024-05-17 04:00:39 +0900

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 const dbConfig = {
     host: "db-react-crud.cda008acygpk.ap-southeast-2.rds.amazonaws.com",
-    port : 8080,
+    port : 3306,
     user: "naimnaro",
     password: "dlwndvy123!",
     database: "account" // 적절한 데이터베이스 이름으로 변경해야 합니다.
