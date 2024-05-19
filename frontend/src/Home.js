bb1b907c5509cda8899001d69cec7c7b19874a8c
naim_naro <xez8jf@gmail.com>
2024-05-19 10:06:53 +0900

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,7 +6,7 @@ axios.defaults.withCredentials = true;
 function Home({ user, setUser }) {
 
     const navigate = useNavigate();
-    
+
     useEffect(() => {
         console.log("User:", user);
 
@@ -19,18 +19,18 @@ function Home({ user, setUser }) {
         navigate('/');
     };
 
-   
+
 
     const handletopagenation = () => {
-        navigate('/pagenation'); 
+        navigate('/pagenation');
     };
 
-   /* const handletotestmodal = () => {
-        navigate('/testmodal');
-    }; 
-     const handletoboard = () => {
-        navigate('/postlist');
-    };*/
+    /* const handletotestmodal = () => {
+         navigate('/testmodal');
+     }; 
+      const handletoboard = () => {
+         navigate('/postlist');
+     };*/
 
     return (
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
@@ -40,6 +40,9 @@ function Home({ user, setUser }) {
                     <div className="d-flex flex-column">
                         <button className="btn btn-success mb-2" onClick={handletopagenation}>자유 게시판</button>
                         <button className="btn btn-danger mb-2" onClick={handleLogout}>로그아웃</button>
+                        <button className="btn btn-info mb-2" onClick={() => window.location.href = "https://www.jungpyo.info/"}>개발자 홈페이지</button>
+                        <button className="btn btn-info mb-2" onClick={() => window.location.href = "https://www.instagram.com/naim_naro/"}>개발자 SNS</button>
+
                     </div>
                 </div>
             )}
