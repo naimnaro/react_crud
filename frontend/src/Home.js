f4909e01a201418ff78280d2f3f07310bbd743da
naim_naro <xez8jf@gmail.com>
2024-05-19 09:40:48 +0900

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -19,17 +19,18 @@ function Home({ user, setUser }) {
         navigate('/');
     };
 
-    const handletoboard = () => {
-        navigate('/postlist');
-    };
+   
 
     const handletopagenation = () => {
         navigate('/pagenation'); 
     };
 
-    const handletotestmodal = () => {
+   /* const handletotestmodal = () => {
         navigate('/testmodal');
-    };
+    }; 
+     const handletoboard = () => {
+        navigate('/postlist');
+    };*/
 
     return (
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
@@ -37,10 +38,8 @@ function Home({ user, setUser }) {
                 <div className="text-center border rounded bg-white p-4">
                     <p className="mb-4" style={{ fontSize: '24px' }}>환영합니다, {user.name}님!</p>
                     <div className="d-flex flex-column">
-                        <button className="btn btn-success mb-2" onClick={handletoboard}>자유 게시판(카드박스 버전)</button>
-                        <button className="btn btn-success mb-2" onClick={handletopagenation}>자유 게시판(페이징 버전)</button>
+                        <button className="btn btn-success mb-2" onClick={handletopagenation}>자유 게시판</button>
                         <button className="btn btn-danger mb-2" onClick={handleLogout}>로그아웃</button>
-                        <button className="btn btn-danger mb-2" onClick={handletotestmodal}>모달 테스트</button>
                     </div>
                 </div>
             )}
